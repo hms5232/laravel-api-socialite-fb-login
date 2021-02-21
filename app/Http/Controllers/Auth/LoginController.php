266ee2c3b666eae493561fdc2167c8590f35de4b
hms5232 <hms5232@yahoo.com.tw>
2021-02-21 21:15:19 +0800

    API for FB login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -35,6 +43,65 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+//        $this->middleware('guest')->except('logout');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Facebook Login
+    |--------------------------------------------------------------------------
+    */
+    public function fbLogin(Request $request)
+    {
+        $redirect_url = Socialite::driver('facebook')
+            ->scopes(['email'])  // 額外要求要使用者的電子信箱地址
+            ->redirect()->getTargetUrl();
+
+        return response()->json(['target' => [$redirect_url]], 302);
+    }
+
+    public function fbLoginCallback(Request $request)
+    {
+        if (is_null($request['code']) || is_null($request['state'])){
+            return response()->json(['messages' => ['授權失敗']], 401);
+        }
+
+        Session::put('state', $request['state']);  // 不寫入 session 就要 stateless
+
+        try{
+            $fb_user = Socialite::driver('facebook')->user();
+        } catch (\Exception $exception){
+            Log::error($exception);
+            return response()->json(['messages' => [strval($exception)]], 401);
+        }
+
+        // 確認使用者是否已經使用此方法註冊過
+        if (User::where('fb_id', $fb_user->getId())->exists()){  // 有
+            // 登入
+            Auth::guard('web')->login(User::where('fb_id', $fb_user->getId())->first());
+        } else if (User::where('email', $fb_user->getEmail())->exists()){  // 有相同 email 的使用者
+            // 更新使用者資料
+            DB::transaction(function () use ($fb_user){
+                User::where('email', $fb_user->getEmail())->update([
+                    'fb_id' => $fb_user->getId()
+                ]);
+            });
+
+            Auth::guard('web')->login(User::where('email', $fb_user->getEmail())->first());
+        }else {  // 沒找到
+            // 自動註冊
+            DB::transaction(function () use ($fb_user){
+                User::create([
+                    'name' => $fb_user->getName(),
+                    'email' => $fb_user->getEmail(),
+                    'password' => Hash::make(uniqid('FB_')),
+                    'fb_id' => $fb_user->getId()
+                ]);
+            });
+
+            Auth::guard('web')->login(User::where('email', $fb_user->getEmail())->where('fb_id', $fb_user->getId())->first());
+        }
+
+        return response()->json(['messages' => ['登入成功！']], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+//Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+/*
+ * 請從這邊開始看，上面是自動產生的
+ *
+ * 建議註解 Auth 那行，避免不必要的影響
+ */
+
+// FB 登入
+Route::get('/auth/facebook-login', [App\Http\Controllers\Auth\LoginController::class, 'fbLogin']);
+
+// FB 登入 callback
+Route::get('/auth/facebook-login-callback', [App\Http\Controllers\Auth\LoginController::class, 'fbLoginCallback']);
